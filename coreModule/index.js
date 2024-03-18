57c1ca402c20057690168f69e9916b031ed2373e
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-18 14:17:00 +0000

    add crypto

--- coreModule/index.js
+++ coreModule/index.js
@@ -1,3 +1,22 @@
 const crypto1 = require("crypto")
+
+// randomInt()
 const ranNum = crypto1.randomInt(100)
 console.log(ranNum)
+
+// randomBytes()
+const ranBytes = crypto1.randomBytes(10).toString('hex')
+const ranBytes2 = crypto1.randomBytes(10).toString('base64')
+const ranBytes3 = crypto1.randomBytes(10).toString('utf-8')
+const ranBytes4 = crypto1.randomBytes(10).toString("utf-16le")
+
+console.log(ranBytes)
+console.log(ranBytes2)
+console.log(ranBytes3)
+console.log(ranBytes4)
+
+// randomUUID
+const ranUUID = crypto1.randomUUID()
+console.log(ranUUID)
+
+// encrypt and decrypt on crypto
