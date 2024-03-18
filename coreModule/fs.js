c0075a6f22a81cf4fc44ebd3046753d7a1e64195
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-18 14:45:14 +0000

    add callback

---
+++ coreModule/fs.js
@@ -0,0 +1,31 @@
+// create file with type nul > filename
+// type nul > fileName
+
+// callback
+
+function add(a,b){
+    const results = a * b
+    console.log(results)
+}
+
+function callingCallback(a,b,callbacks){
+    callbacks(a,b)
+}
+
+callingCallback(2,6, add)
+
+callingCallback(56,100, function(a,b){
+    const results = a - b
+    console.log(results)
+})
+
+callingCallback(2134, 3432,function(a,b){
+    const results = a / b
+    console.log(results)
+})
+
+callingCallback(13334, 13412, (a,b) => {
+    const results = a + b
+    console.log(results)
+})
+
