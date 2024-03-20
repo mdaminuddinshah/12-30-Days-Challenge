04fc9ed48fec05fcc6ff0c8b183b912e829de0f2
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-21 01:02:56 +0800

    http 301 for redirection

--- Http/http.js
+++ Http/http.js
@@ -17,7 +17,16 @@ server1.listen(8080)
 const server2 = http.createServer((req,res) => {
     res.writeHead(200, {"content-type": "application/json"});
     res.write(JSON.stringify({name: "amin", month: "august"}));
+    // res.writeHead(301, {Location:"https://www.youtube.com"})
     res.end()
 })
-server2.listen(8888)
+server2.listen(8880)
+
+// make redirectional ON 8880
+const server3 = http.createServer((req,res) => {
+    res.writeHead(301, {Location: "https://www.google.com"});
+    res.end;
+})
+
+server3.listen(8800)
 
