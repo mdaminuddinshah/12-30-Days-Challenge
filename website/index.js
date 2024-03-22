dbf0e19dc9a1abb677ecb91156f8e9c8d19343dd
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-23 02:24:33 +0800

    chang index.js

---
+++ website/index.js
@@ -0,0 +1,46 @@
+const http = require("http")
+const fs = require("fs")
+const path = require("path")
+
+const filePath1 = path.join(__dirname, "./pages", "./index.html")
+const filePath2 = path.join(__dirname, "./pages", "./project.html")
+const filePath3 = path.join(__dirname, "./pages", "./error.html")
+
+const readFile1 = fs.readFileSync(filePath1,"utf-8")
+const readFile2 = fs.readFileSync(filePath2,"utf-8")
+const readFile3 = fs.readFileSync(filePath3,"utf-8")
+
+const server1 = http.createServer((req,res) => {
+    
+    const url = req.url.toLowerCase()
+    console.log(url)
+
+    if(url == "/"){
+        res.writeHead(200, {"content-type": "text/html"})
+        res.write(readFile1)
+        res.end()
+        console.log(readFile1)
+        return
+    }
+
+    if(url == "/project"){
+        res.writeHead(200, {"content-type": "text/html"})
+        res.write(readFile2)
+        res.end()
+        console.log(readFile2)
+        return
+    }
+
+    else{
+        res.writeHead(200, {"content-type": "text/html"})
+        res.write(readFile3)
+        res.end()
+        console.log(readFile3)
+        return
+    }
+    
+})
+
+server1.listen(1234, function(){
+    console.log("the server 1234 is running")
+})
