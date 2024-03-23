7473d4cc5be5196ef353c2b3239a153c7ccbdd2f
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-24 01:52:58 +0800

    pages with express js

--- expressJs/index.js
+++ expressJs/index.js
@@ -4,12 +4,22 @@ const path = require("path")
 
 const app = express()
 const indexHtmlPath = path.join(__dirname, "./pages", "./index.html")
+const formIndexPath = path.join(__dirname, "./pages", "./bmi-form.html")
+
 const indexHtmlFile = fs.readFileSync(indexHtmlPath, "utf-8")
+const bmiFormFile = fs.readFileSync(formIndexPath, "utf-8")
+
+app.use(express.static("./public"))
 
 app.get("/", function(req,res){
     res.setHeader("content-type", "text/html");
     res.send(indexHtmlFile);
-    // console.log(indexHtmlFile)
+    console.log(indexHtmlFile);
+})
+
+app.get("/bmi-form.html", function(req, res){
+    res.setHeader("content-type", "text/html");
+    res.send(bmiFormFile);
 })
 
 app.listen(1001)
