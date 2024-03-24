8b3e965c66217b64aa1d654dd7b98a6d2aea64b0
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-24 23:46:27 +0800

    create route post

--- expressJs/index.js
+++ expressJs/index.js
@@ -5,21 +5,33 @@ const path = require("path")
 const app = express()
 const indexHtmlPath = path.join(__dirname, "./pages", "./index.html")
 const formIndexPath = path.join(__dirname, "./pages", "./bmi-form.html")
+const resultIndexPath = path.join(__dirname, "./pages", "./bmi-result.html")
 
 const indexHtmlFile = fs.readFileSync(indexHtmlPath, "utf-8")
 const bmiFormFile = fs.readFileSync(formIndexPath, "utf-8")
+const resultFormFile = fs.readFileSync(resultIndexPath, "utf-8")
 
+// untuk serve static file = css,js, images
+// guna express.static(root folder)
 app.use(express.static("./public"))
 
 app.get("/", function(req,res){
     res.setHeader("content-type", "text/html");
     res.send(indexHtmlFile);
-    console.log(indexHtmlFile);
 })
 
 app.get("/bmi-form.html", function(req, res){
     res.setHeader("content-type", "text/html");
     res.send(bmiFormFile);
+})
+
+app.get("/bmi-result.html", function(req,res){
+    res.setHeader("content-type", "text/html")
+    res.send(resultFormFile)
+})
+
+app.post("/calculate", function(req,res){
+    res.redirect("/bmi-result.html")
 })
 
 app.listen(1001)
